0c43bf164e8cbda944c7ff6929348b078c16e582
Wandy <Wan20@users.noreply.github.com>
2016-05-20 14:10:58 +0700

    Add files via upload

---
+++ Chapter1_CSharp/Chapter1_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
